fb587d09d710734e7441c959aa332a8dbe0f9b95
xeaza <taylorwrightsanson@gmail.com>
2014-10-06 17:06:12 -0500

    Got checkmarks to toggle

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -11,23 +11,27 @@
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *addToDoItemTextField;
-
-@property (weak, nonatomic) IBOutlet UITableView *toDoListTableView;
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property NSMutableArray *checkedIndexes;
 
 @property NSMutableArray *todoListArray;
 
-@end
+@end        // Update checkmarked buttson list
 
 @implementation ViewController
-
-
-
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     self.todoListArray = [NSMutableArray arrayWithObjects:@"Sky Dive from Sears Tower", @"Eat Cake", @"Tickle a Dragon", @"Don't Panic", nil];
+
+    self.checkedIndexes = [NSMutableArray arrayWithCapacity:self.todoListArray.count];
+    // Created array of 0s indicating that no cells have been selected (i.e. no checkmark added)
+    for (int i = 0; i < self.todoListArray.count; i++)
+    {
+        [self.checkedIndexes addObject:[NSNumber numberWithBool:NO]];
+    }
+
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -42,23 +46,35 @@
     return cell;
 }
 
-
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"toDoListCell" forIndexPath:indexPath];
-    cell.accessoryType = UITableViewCellAccessoryCheckmark;
-  //  [self.toDoListTableView deselectRowAtIndexPath:indexPath animated:YES];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    //cell.accessoryType = UITableViewCellAccessoryCheckmark;
+   [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+    if (cell.accessoryType == UITableViewCellAccessoryNone)
+    {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        // Update checkmarked buttson list
+        [self.checkedIndexes replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:YES]];
+
+    }
+    else
+    {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        [self.checkedIndexes removeObject:indexPath];
+        // Update checkmarked buttson list
+        [self.checkedIndexes replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:NO]];
+    }
 }
-
-
 
 - (IBAction)onAddButtonPressed:(id)sender {
     NSString *usersText = self.addToDoItemTextField.text;
     [self.todoListArray addObject:usersText];
-    //[self.toDoListTableView reloadData];
-    [self.toDoListTableView beginUpdates];
-    [self.toDoListTableView insertRowsAtIndexPaths:self.todoListArray withRowAnimation:UITableViewRowAnimationFade];
-    [self.toDoListTableView endUpdates];
+    [self.checkedIndexes addObject:[NSNumber numberWithBool:NO]];
+    [self.tableView reloadData];
+
+
     self.addToDoItemTextField.text = nil;
     [self.addToDoItemTextField resignFirstResponder];
 }
