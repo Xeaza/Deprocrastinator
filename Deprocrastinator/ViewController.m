bb06c1ab6fdb9ad6a6b683138a3ba0cea105e5d5
xeaza <taylorwrightsanson@gmail.com>
2014-10-27 09:26:24 -0500

    Refactored so that code was cleaner and now the checked and colored cells stay checked and colored correctly when user scrolls and cells are reused.

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -14,6 +14,8 @@
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property NSMutableArray *checkedIndexes;
+@property NSMutableArray *colorIndexPaths;
+
 @property NSMutableArray *todoListArray;
 @property NSIndexPath *indexToDelete;
 @property NSArray *checkedItemsArray;
@@ -22,91 +24,84 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-
-    //self.checkedItemsArray = @[@{@"itemName" : @"Sky Dive from Sears Tower", @"itemIndex" : @0}];
-    //NSLog(@"%@", [[self.checkedItemsArray objectAtIndex:0] valueForKey:@"itemName"]);
-
+    
     self.indexToDelete = [[NSIndexPath alloc] init];
     self.todoListArray = [NSMutableArray arrayWithObjects:@"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Sky Dive from Sears Tower", @"Eat Cake", @"Tickle a Dragon", @"Don't Panic", nil];
 
-    // TODO add array that keeps track of which cells are checked
-    // TODO add array that keeps track of which cells have their text color different
-    // Use these arrays to reset the cells to have the correct accessories when they are reused.
-
-    self.checkedIndexes = [NSMutableArray arrayWithCapacity:self.todoListArray.count];
-    // Created array of 0s indicating that no cells have been selected (i.e. no checkmark added)
+    self.colorIndexPaths = [[NSMutableArray alloc] init];
+    self.checkedIndexes = [[NSMutableArray alloc] init];
+    // Created array of 0s indicating that no cells have been selected (i.e. no checkmark added) as well as an array of text indicating the color of the textLabels
     for (int i = 0; i < self.todoListArray.count; i++)
     {
         [self.checkedIndexes addObject:[NSNumber numberWithBool:NO]];
+        [self.colorIndexPaths addObject:@"Black"];
     }
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
     return self.todoListArray.count;
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
 
     NSString *cellIdentifier = @"toDoListCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
-
-    // NOTE: Cells should only be minupulated in cellForRowAtIndexPath *****************************************
-
-    // TODO
-    // Use the arrays holding which cells have check marks and changed text color here to updated the cells to
-    // show the correct information
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                reuseIdentifier:cellIdentifier];
-    }
-
-    //Refresh acessory for cell when tableview have many cells and reuse identifier
-    /*if([self.selectedIndexes objectAtIndex:indexPath.row]){
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    }else{
-        cell.accessoryType = UITableViewCellAccessoryNone;
-    }*/
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
 
     cell.textLabel.text = [self.todoListArray objectAtIndex:indexPath.row];
+    [cell setAccessoryType:UITableViewCellAccessoryNone];
+
+    BOOL shouldBeChecked = [[self.checkedIndexes objectAtIndex:indexPath.row] boolValue];
+
+    if (shouldBeChecked)
+    {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+
+    if (self.colorIndexPaths)
+    {
+
+        if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"Black"])
+        {
+            cell.textLabel.textColor = [UIColor blackColor];
+        } else if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"Green"])
+        {
+            cell.textLabel.textColor = [UIColor greenColor];
+        } else if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"Yellow"])
+        {
+            cell.textLabel.textColor = [UIColor yellowColor];
+        } else if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"Red"])
+        {
+            cell.textLabel.textColor = [UIColor redColor];
+        }
+    }
+
     return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    //cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    BOOL currentValue = [[self.checkedIndexes objectAtIndex:indexPath.row] boolValue];
+    BOOL updatedValue = !currentValue;
 
-    // TODO - Don't update the cells here update them in cell for row at index path.
-    // remove the cell.accessoryType = something
-    // make sure to just update the data model (i.e. arrays) so that when cellsForRowAtIndexPath is called
-    // when you reload the data it updates with correct information.
-   [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    self.checkedIndexes[indexPath.row] = [NSNumber numberWithBool:updatedValue];
 
-    if (cell.accessoryType == UITableViewCellAccessoryNone)
-    {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-        // Update checkmarked buttson list
-        [self.checkedIndexes replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:YES]];
-
-    }
-    else
-    {
-        cell.accessoryType = UITableViewCellAccessoryNone;
-        [self.checkedIndexes removeObject:indexPath];
-        // Update checkmarked buttson list
-        [self.checkedIndexes replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:NO]];
-    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [tableView reloadData];
 }
 
--(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
     return YES;
 }
 
--(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
-
-
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+ //
 }
 
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
@@ -116,50 +111,63 @@
     }
 }
 
-- (IBAction)onAddButtonPressed:(id)sender {
+- (IBAction)onAddButtonPressed:(id)sender
+{
     NSString *usersText = self.addToDoItemTextField.text;
     [self.todoListArray addObject:usersText];
     [self.checkedIndexes addObject:[NSNumber numberWithBool:NO]];
     [self.tableView reloadData];
 
-
     self.addToDoItemTextField.text = nil;
     [self.addToDoItemTextField resignFirstResponder];
 }
-- (IBAction)onEditButtonPressed:(UIButton *)editButton {
-    if ([editButton.titleLabel.text isEqualToString:@"Edit"]) {
+
+- (IBAction)onEditButtonPressed:(UIButton *)editButton
+{
+    if ([editButton.titleLabel.text isEqualToString:@"Edit"])
+    {
         [editButton setTitle:@"Done" forState:UIControlStateNormal];
-        // Add Logic to delete a rows
-
+        // TODO Add Logic to delete a rows
         [self.tableView setEditing:YES animated:YES];
-
-    } else {
+    }
+    else
+    {
         [editButton setTitle:@"Edit" forState:UIControlStateNormal];
         [self.tableView setEditing:NO animated:YES];
-
     }
 }
 
-- (IBAction)onSwipeGesture:(UISwipeGestureRecognizer *)swipeGesture {
+- (IBAction)onSwipeGesture:(UISwipeGestureRecognizer *)swipeGesture
+{
     CGPoint point = [swipeGesture locationInView:self.tableView];
     NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:point];
 
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-    if (cell.textLabel.textColor == [UIColor blackColor]) {
+    if (cell.textLabel.textColor == [UIColor blackColor])
+    {
         cell.textLabel.textColor = [UIColor greenColor];
+        [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"Green"];
     }
-    else if (cell.textLabel.textColor == [UIColor greenColor]){
+    else if (cell.textLabel.textColor == [UIColor greenColor])
+    {
         cell.textLabel.textColor = [UIColor yellowColor];
+        [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"Yellow"];
     }
-    else if (cell.textLabel.textColor == [UIColor yellowColor]) {
+    else if (cell.textLabel.textColor == [UIColor yellowColor])
+    {
         cell.textLabel.textColor = [UIColor redColor];
+        [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"Red"];
     }
-    else if (cell.textLabel.textColor == [UIColor redColor]) {
+    else if (cell.textLabel.textColor == [UIColor redColor])
+    {
         cell.textLabel.textColor = [UIColor blackColor];
+        [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"Black"];
     }
+
 }
 
-- (void)showDeleteCheckAlert: (NSIndexPath *)indexPath {
+- (void)showDeleteCheckAlert: (NSIndexPath *)indexPath
+{
     UIAlertView *alertView = [[UIAlertView alloc] init];
     alertView.delegate = self;
     self.indexToDelete = indexPath;
@@ -169,7 +177,8 @@
     [alertView show];
 }
 
--(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
 
     if (buttonIndex == 0 )
     {
@@ -184,7 +193,8 @@
     }
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
